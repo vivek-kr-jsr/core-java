d272d8c4191414ddf3423491c129efc587fe0217
vivekkumar08 <sonusingh29051998@gmail.com>
2024-05-23 23:12:57 +0530

    Initial changes

--- src/com/collection/queue/MyArrayDeque.java
+++ src/com/collection/queue/MyArrayDeque.java
@@ -1,4 +1,139 @@
 package com.collection.queue;
 
-public class MyArrayDeque {
+import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.Queue;
+
+public class MyArrayDeque<E> implements Queue<E> {
+    private E[] elements;
+    private int head;
+    private int tail;
+    private final static int DEFAULT_CAPACITY=8;
+
+    public MyArrayDeque(){
+        this(DEFAULT_CAPACITY);
+    }
+
+    public MyArrayDeque(int defaultCapacity) {
+        elements =(E[]) new Object[];
+        head = tail =0;
+    }
+
+    @Override
+    public int size() {
+        return (tail - head) & (elements.length - 1);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == tail;;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if(o == null) return false;
+        int mask = elements.length - 1;
+        int i = head;
+        Object x;
+        while ( (x = elements[i]) != null) {
+            if (o.equals(x))
+                return true;
+            i = (i + 1) & mask;
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        int size = size();
+        if (a.length < size)
+            a = (T[])java.lang.reflect.Array.newInstance(
+                    a.getClass().getComponentType(), size);
+        copyElements(a);
+        if (a.length > size)
+            a[size] = null;
+        return a;
+    }
+
+    private <T> T[] copyElements(T[] a) {
+        if (head < tail) {
+            System.arraycopy(elements, head, a, 0, size());
+        } else if (head > tail) {
+            int headPortionLen = elements.length - head;
+            System.arraycopy(elements, head, a, 0, headPortionLen);
+            System.arraycopy(elements, 0, a, headPortionLen, tail);
+        }
+        return a;
+    }
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean offer(E e) {
+        return false;
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E poll() {
+        return null;
+    }
+
+    @Override
+    public E element() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
 }
